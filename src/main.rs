9d1f784220527b2f8bfb04ffebabafc267d5a801
Rodrigo Parra <rodpar07@gmail.com>
2025-11-01 09:07:22 -0300

    Smooth movement

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,28 @@ use macroquad::prelude::*;
 
 #[macroquad::main("My Game")]
 async fn main() {
+    const MOVEMENT_SPEED: f32 = 200.0;
     let mut x = screen_width() / 2.0;
     let mut y = screen_height() / 2.0;
 
     loop {
         clear_background(DARKPURPLE);
+        let delta_time = get_frame_time();
         if is_key_down(KeyCode::Right) {
-            x += 1.0;
+            x += MOVEMENT_SPEED * delta_time;
         }
         if is_key_down(KeyCode::Left) {
-            x -= 1.0;
+            x -= MOVEMENT_SPEED * delta_time;
         }
         if is_key_down(KeyCode::Down) {
-            y += 1.0;
+            y += MOVEMENT_SPEED * delta_time;
         }
         if is_key_down(KeyCode::Up) {
-            y -= 1.0;
+            y -= MOVEMENT_SPEED * delta_time;
         }
+
+        x = clamp(x, 0.0, screen_width());
+        y = clamp(y, 0.0, screen_height());
 
         draw_circle(x, y, 16.0, YELLOW);
         next_frame().await
