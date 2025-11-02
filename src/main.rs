87679f55d795bbc111f4a588a15e0f2dbe0b2bc6
Rodrigo Parra <rodpar07@gmail.com>
2025-11-01 22:29:57 -0300

    Bullet hell

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ struct Shape {
     x: f32,
     y: f32,
     color: Color,
+    collided: bool,
 }
 
 impl Shape {
@@ -39,12 +40,14 @@ async fn main() {
     const MOVEMENT_SPEED: f32 = 200.0;
     let colors = [GREEN, BLUE, RED];
     let mut squares = vec![];
+    let mut bullets = vec![];
     let mut circle = Shape {
         size: 32.0,
         speed: MOVEMENT_SPEED,
         x: screen_width() / 2.0,
         y: screen_height() / 2.0,
         color: YELLOW,
+        collided: false,
     };
 
     let mut gameover = false;
@@ -66,10 +69,23 @@ async fn main() {
             if is_key_down(KeyCode::Up) {
                 circle.y -= MOVEMENT_SPEED * delta_time;
             }
+            if is_key_pressed(KeyCode::Space) {
+                bullets.push(Shape {
+                    x: circle.x,
+                    y: circle.y,
+                    speed: circle.speed * 2.0,
+                    size: 5.0,
+                    collided: false,
+                    color: RED,
+                });
+            }
         }
         circle.x = clamp(circle.x, 0.0, screen_width());
         circle.y = clamp(circle.y, 0.0, screen_height());
 
+        for bullet in &bullets {
+            draw_circle(bullet.x, bullet.y, bullet.size / 2.0, bullet.color);
+        }
         draw_circle(circle.x, circle.y, circle.size / 2.0, YELLOW);
 
         if rand::gen_range(0, 99) >= 95 {
@@ -80,6 +96,7 @@ async fn main() {
                 x: rand::gen_range(size / 2.0, screen_width() - size / 2.0),
                 y: -size,
                 color: *colors.choose().unwrap(),
+                collided: false,
             });
         }
 
@@ -87,9 +104,16 @@ async fn main() {
             for square in &mut squares {
                 square.y += square.speed * delta_time;
             }
+            for bullet in &mut bullets {
+                bullet.y -= bullet.speed * delta_time;
+            }
         }
 
         squares.retain(|square| square.y < screen_height() + square.size);
+        bullets.retain(|bullet| bullet.y > 0.0 - bullet.size / 2.0);
+
+        squares.retain(|square| !square.collided);
+        bullets.retain(|bullet| !bullet.collided);
 
         for square in &squares {
             draw_rectangle(
@@ -105,8 +129,18 @@ async fn main() {
             gameover = true;
         }
 
+        for square in squares.iter_mut() {
+            for bullet in bullets.iter_mut() {
+                if bullet.collides_with(square) {
+                    bullet.collided = true;
+                    square.collided = true;
+                }
+            }
+        }
+
         if gameover && is_key_pressed(KeyCode::Space) {
             squares.clear();
+            bullets.clear();
             circle.x = screen_width() / 2.0;
             circle.y = screen_height() / 2.0;
             gameover = false;
